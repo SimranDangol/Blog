e457e6dcfe7b69ae5ec7351a11a54c7459215a28
Simran Dangol <simrandangol10@gmail.com>
2024-11-19 12:28:10 +0545

    first commit

--- server/app.js
+++ server/app.js
@@ -2,12 +2,18 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import admin from "firebase-admin";
+import path from "path";
+import { fileURLToPath } from "url";
 
 //Routes
 import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
 import postRouter from "./routes/post.route.js";
 import commentRouter from "./routes/comment.route.js";
+
+// Define __filename and __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -17,6 +23,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 const corsOptions = { origin: process.env.URL, credentials: true };
 app.use(cors(corsOptions));
+app.use(express.static("public"));
 
 // Initialize Firebase Admin
 admin.initializeApp({
@@ -28,5 +35,13 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/post", postRouter);
 app.use("/api/v1/comment", commentRouter);
+
+// Serve static files from the frontend/dist directory
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  });
+  
 
 export default app;
