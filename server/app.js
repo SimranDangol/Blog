f0753780713e4da029ba24497b87c42562a5e6dd
Simran Dangol <simrandangol10@gmail.com>
2024-11-20 12:49:03 +0545

    first commit

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import admin from "firebase-admin";
 import path from "path";
-import { fileURLToPath } from "url";
+
 
 //Routes
 import authRouter from "./routes/auth.route.js";
@@ -11,8 +11,7 @@ import userRouter from "./routes/user.route.js";
 import postRouter from "./routes/post.route.js";
 import commentRouter from "./routes/comment.route.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -35,12 +34,10 @@ app.use("/api/v1/user", userRouter);
 app.use("/api/v1/post", postRouter);
 app.use("/api/v1/comment", commentRouter);
 
-// // Serve static files from client/dist
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
-// // Catch-all handler for SPA
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  })
 
 export default app;
