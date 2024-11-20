352a85ea028d9d10fb96ddbd2d418407cbca065b
Simran Dangol <simrandangol10@gmail.com>
2024-11-20 13:29:28 +0545

    first commit

--- server/app.js
+++ server/app.js
@@ -4,40 +4,40 @@ import cors from "cors";
 import admin from "firebase-admin";
 import path from "path";
 
-
-//Routes
+// Routes
 import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
 import postRouter from "./routes/post.route.js";
 import commentRouter from "./routes/comment.route.js";
 
 const __dirname = path.resolve();
-
 const app = express();
 
-//Middlewares
+// Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
 const corsOptions = { origin: process.env.URL, credentials: true };
 app.use(cors(corsOptions));
-app.use(express.static("public"));
 
 // Initialize Firebase Admin
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
 });
 
-//Routes
+// API Routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/post", postRouter);
 app.use("/api/v1/comment", commentRouter);
 
+// Serve static files from the frontend build folder
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
+// Catch-all route to serve the frontend index.html for SPA routing
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-  })
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 export default app;
